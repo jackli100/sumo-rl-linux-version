5f3a9b32b9189a3b53448849470e5b2f8515fe72
jackli100 <1811743445@qq.com>
2024-06-28 23:54:15 +0800

    0628

--- experiments/multi_mission.py
+++ experiments/multi_mission.py
@@ -1,4 +1,5 @@
 import subprocess
+from multiprocessing import Pool
 
 def run_program(command):
     # 运行命令
@@ -7,13 +8,22 @@ def run_program(command):
 if __name__ == "__main__":
     # 定义要运行的命令和参数
     commands = [
-        ["python", r"D:\trg1vr\sumo-rl-main\sumo-rl-main\experiments\train_class2.py",
-         '--num_of_episodes', '10',
-         '--net_path', r"D:\trg1vr\sumo-rl-main\sumo-rl-main\sumo_rl\nets\2way-single-intersection\single-intersection-3.net.xml",
-         '--total_timesteps', '500000',
-         '--proportion_of_saturations', '0.5,0.5,0.5,0.5'],          
+        # 17-stage (net-2)
+        ["python", "experiments/train_class2.py",
+         '--num_of_episodes', '9',
+         '--net_path', "sumo_rl/nets/2way-single-intersection/single-intersection-4.net.xml",
+         '--total_timesteps', '900000',
+         '--fix_ts',
+         '--proportion_of_saturations', '0.75,0.75,0.75,0.75',  
+         '--note', 'intersection-4, 0.75 saturation, fix ts'],
+        ["python", "experiments/train_class2.py",
+         '--num_of_episodes', '9',
+         '--net_path', "sumo_rl/nets/2way-single-intersection/single-intersection-3.net.xml",
+         '--total_timesteps', '900000',
+         '--fix_ts',
+         '--proportion_of_saturations', '0.75,0.75,0.75,0.75',  
+         '--note', 'intersection-3, 0.75 saturation, fix ts'] 
     ]
-
-    # 运行命令
-    for command in commands:
-        run_program(command)
+    # 使用多进程池并行运行命令
+    with Pool(processes=len(commands)) as pool:
+        pool.map(run_program, commands)
